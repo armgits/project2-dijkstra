05157db6b53eef017ec3fd0d38d6b91adc49e9ec
Abhishekh <126507842+armgits@users.noreply.github.com>
2024-03-16 22:36:44 -0400

    Updated definition of node objects and its attributres

--- src/node_dijkstra.cpp
+++ src/node_dijkstra.cpp
@@ -1,114 +1,135 @@
+/**
+ * @file node_dijkstra.cpp
+ * @author Abhishekh Reddy (areddy42@umd.edu)
+ * @brief Implementation of node object and more
+ * @version 1.0
+ * @date 2024-03-16
+ *
+ * @copyright Copyright (c) 2024 Abhishekh Reddy
+ *
+ */
+
 #include "node_dijkstra.hpp"
 
-project2::NodeDijkstra::NodeDijkstra()
-: index_ {0},
-  parent_index_ {0},
-  cost_to_come_ {0},
-  position_ {0, 0} {}
+project2::Node::Node()
+: position_ {0, 0},
+  from_position_ {0, 0},
+  distance_ {0} {}
 
-project2::NodeDijkstra::NodeDijkstra(
-  int index,
-  int parent_index,
-  float cost_to_come,
-  const Position& position
+project2::Node::Node(
+  const project2::Position& position
 )
-: index_ {index},
-  parent_index_ {parent_index},
-  cost_to_come_ {cost_to_come},
-  position_ {position} {}
+: position_ {position},
+  from_position_ {},
+  distance_ {} {}
 
-void project2::NodeDijkstra::actionMove(
-  Action action,
-  std::vector<NodeDijkstra>& children_nodes)
+project2::Node::Node(
+  const project2::Position& position,
+  const project2::Position& from_position,
+  float distance
+)
+: position_ {position},
+  from_position_ {from_position},
+  distance_ {distance} {}
+
+bool project2::Node::actionMove(
+  project2::Action action,
+  project2::Node& child_node)
 {
-  float new_cost_to_come {cost_to_come_};
-  Position new_position {position_};
+  float new_distance {distance_};
+  project2::Position new_position {position_};
 
   switch (action)
   {
-  case (Action::UP):
+  case (project2::Action::UP):
       if (new_position.y >= Y_MAX_MM)
-        return;
+        return false;
 
       new_position.y += ACTION_DISPLACEMENT_MM;
-      new_cost_to_come += ACTION_COST_STRAIGHT;
+      new_distance += ACTION_COST_STRAIGHT;
       break;
 
-  case (Action::UP_RIGHT):
+  case (project2::Action::UP_RIGHT):
       if (new_position.y >= Y_MAX_MM || new_position.x >= X_MAX_MM)
-        return;
+        return false;
 
       new_position.y += ACTION_DISPLACEMENT_MM;
       new_position.x += ACTION_DISPLACEMENT_MM;
-      new_cost_to_come += ACTION_COST_DIAGONAL;
+      new_distance += ACTION_COST_DIAGONAL;
       break;
 
-  case (Action::RIGHT):
+  case (project2::Action::RIGHT):
       if (new_position.x >= X_MAX_MM)
-        return;
+        return false;
 
       new_position.x += ACTION_DISPLACEMENT_MM;
-      new_cost_to_come += ACTION_COST_STRAIGHT;
+      new_distance += ACTION_COST_STRAIGHT;
       break;
 
-  case (Action::DOWN_RIGHT):
+  case (project2::Action::DOWN_RIGHT):
       if (new_position.y <= Y_MIN_MM || new_position.x >= X_MAX_MM)
-        return;
+        return false;
 
       new_position.y -= ACTION_DISPLACEMENT_MM;
       new_position.x += ACTION_DISPLACEMENT_MM;
-      new_cost_to_come += ACTION_COST_DIAGONAL;
+      new_distance += ACTION_COST_DIAGONAL;
       break;
 
-  case (Action::DOWN):
+  case (project2::Action::DOWN):
       if (new_position.y <= Y_MIN_MM)
-        return;
+        return false;
 
       new_position.y -= ACTION_DISPLACEMENT_MM;
-      new_cost_to_come += ACTION_COST_STRAIGHT;
+      new_distance += ACTION_COST_STRAIGHT;
       break;
 
-  case (Action::DOWN_LEFT):
+  case (project2::Action::DOWN_LEFT):
       if (new_position.y <= Y_MIN_MM || new_position.x <= X_MIN_MM)
-        return;
+        return false;
 
       new_position.y -= ACTION_DISPLACEMENT_MM;
-      new_position.y -= ACTION_DISPLACEMENT_MM;
-      new_cost_to_come += ACTION_COST_DIAGONAL;
+      new_position.x -= ACTION_DISPLACEMENT_MM;
+      new_distance += ACTION_COST_DIAGONAL;
       break;
 
-  case (Action::LEFT):
+  case (project2::Action::LEFT):
       if (new_position.x <= X_MIN_MM)
-        return;
+        return false;
 
       new_position.x -= ACTION_DISPLACEMENT_MM;
-      new_cost_to_come += ACTION_COST_STRAIGHT;
+      new_distance += ACTION_COST_STRAIGHT;
       break;
 
-  case (Action::UP_LEFT):
+  case (project2::Action::UP_LEFT):
       if (new_position.y >= Y_MAX_MM || new_position.x <= X_MIN_MM)
-        return;
+        return false;
 
       new_position.y += ACTION_DISPLACEMENT_MM;
       new_position.x -= ACTION_DISPLACEMENT_MM;
-      new_cost_to_come += ACTION_COST_DIAGONAL;
+      new_distance += ACTION_COST_DIAGONAL;
       break;
 
   default:
-      return;
+      return false;
   }
 
-  children_nodes.push_back(
-    NodeDijkstra(0, index_, new_cost_to_come, new_position));
+  child_node = project2::Node(new_position, position_, new_distance);
+  return true;
 }
 
-std::vector<project2::NodeDijkstra> project2::NodeDijkstra::getChildrenNodes()
-{
-  std::vector<NodeDijkstra> children_nodes {};
+// std::vector<project2::Node> project2::Node::getChildrenNodes()
+// {
+//   std::vector<project2::Node> children_nodes {};
 
-  for (const auto& action: actions_list) {
-    actionMove(action, children_nodes);
-  }
+//   int index_offset {0};
+//   for (const auto& action: actions_list) {
+//     if (!actionMove(action, children_nodes))
+//       continue;
 
-  return children_nodes;
-}
+//     // TODO: Fix a bug at this line
+//     children_nodes.back().index_ += index_offset;
+//     index_offset++;
+//   }
+
+//   return children_nodes;
+// }
