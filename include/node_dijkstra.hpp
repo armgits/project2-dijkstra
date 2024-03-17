c7f9211b7d99edbd0d682c98037d9ac5aa273d7c
Abhishekh <126507842+armgits@users.noreply.github.com>
2024-03-16 22:34:55 -0400

    Declared node object and its attributes

--- include/node_dijkstra.hpp
+++ include/node_dijkstra.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file node_dijkstra.hpp
+ * @author Abhishekh Reddy (areddy42@umd.edu)
+ * @brief Declaration of the node object and its behavior
+ * @version 0.1
+ * @date 2024-03-16
+ *
+ * @copyright Copyright (c) 2024 Abhishekh Reddy
+ *
+ */
 #pragma once
 
 #include <iostream>
@@ -14,29 +24,6 @@
 #define ACTION_COST_DIAGONAL 1.4
 
 namespace project2 {
-class NodeDijkstra
-{
-  public:
-    NodeDijkstra();
-
-    explicit NodeDijkstra(
-      int index,
-      int parent_index,
-      float cost_to_come,
-      const Position& position);
-
-    std::vector<NodeDijkstra> getChildrenNodes();
-    void setIndex(int index_val) {index_ = index_val;}
-
-  private:
-    void actionMove(Action action, std::vector<NodeDijkstra>& children_nodes);
-
-    int index_;
-    int parent_index_;
-    float cost_to_come_;
-    Position position_;
-};
-
 enum class Action {
   UP = 1,
   UP_RIGHT = 2,
@@ -56,27 +43,104 @@ const std::array<Action, 8> actions_list {
   Action::DOWN,
   Action::DOWN_LEFT,
   Action::LEFT,
-  Action::UP_LEFT};
+  Action::UP_LEFT
+};
 
 struct Position {
-  Position(int x_val, int y_val)
+  Position()
+  : x {0}, y {0} {}
+
+  Position(unsigned int x_val, unsigned int y_val)
   : x {x_val}, y {y_val} {}
 
   Position(const Position& position_val)
   : x {position_val.x},
     y {position_val.y} {}
 
-  bool operator==(const Position& _position)
+  bool operator==(const Position& _position) const
   {
     return (x == _position.x && y == _position.y);
   }
 
-  bool operator!=(const Position& _position)
+  bool operator!=(const Position& _position) const
   {
     return (x != _position.x && y != _position.y);
   }
 
-  int x {0};
-  int y {0};
+  friend std::ostream& operator<<(std::ostream& os, const Position& position)
+  {
+    os << "x: " << position.x << ", y: " << position.y;
+
+    return os;
+  }
+
+  unsigned int x {0};
+  unsigned int y {0};
+};
+
+class Node
+{
+  public:
+    Node();
+
+    explicit Node(
+      const Position& position
+    );
+
+    explicit Node(
+      const Position& position,
+      const Position& from_position,
+      float distance);
+
+    void setDistance(float new_distance) {distance_ = new_distance;}
+    bool actionMove(Action action, Node& child_node);
+
+    const project2::Position& getPosition() const {return position_;}
+    const float getDistance() const {return distance_;}
+    const project2::Position& getFromPosition() const {return from_position_;}
+
+    bool operator==(const Node& compare_node) const
+    {
+      return (position_ == compare_node.position_);
+    }
+
+    bool operator!=(const Node& compare_node) const
+    {
+      return (position_ != compare_node.position_);
+    }
+
+    bool operator>(const Node& compare_node) const
+    {
+      return (distance_ > compare_node.distance_);
+    }
+
+    bool operator>=(const Node& compare_node) const
+    {
+      return (distance_ >= compare_node.distance_);
+    }
+
+    bool operator<(const Node& compare_node) const
+    {
+      return (distance_ < compare_node.distance_);
+    }
+
+    bool operator<=(const Node& compare_node) const
+    {
+      return (distance_ <= compare_node.distance_);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Node& node)
+    {
+      os << "Node " << node.position_ << " ---" << std::endl;
+      os << "From: " << node.from_position_ << std::endl;
+      os << "Distance: " << node.distance_ << std::endl;
+
+      return os;
+    }
+
+  private:
+    float distance_;
+    Position from_position_;
+    Position position_;
 };
 }
